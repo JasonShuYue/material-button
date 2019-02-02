aa2c5cbb89aa0d5b76e6b0272f58f86c523a34b1
Jason_Shu <shuyue524@gmail.com>
2019-02-02 22:58:51 +0800

    npm run build

---
+++ build/precache-manifest.b5aaf413c188d11be0b5ef914071dac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c28f3896b4f2fc59b8c",
+    "url": "/material-button/build/static/js/runtime~main.0c28f389.js"
+  },
+  {
+    "revision": "9dd1b2a4772a69d4a6ee",
+    "url": "/material-button/build/static/js/main.9dd1b2a4.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/material-button/build/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "9dd1b2a4772a69d4a6ee",
+    "url": "/material-button/build/static/css/main.c9c6ba8f.chunk.css"
+  },
+  {
+    "revision": "507e6a0ba329f082ec55afff9ab2aec2",
+    "url": "/material-button/build/index.html"
+  }
+];
